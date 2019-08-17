e1aee9bf53eb626f06641ef5430c10929b696156
Warren Loy Harper II <warrenharper2@att.net>
2019-08-17 21:58:32 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -5,15 +5,8 @@ def hopper
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
         }
+     
      }
 
 
@@ -56,6 +49,7 @@ def dennis_ritchies_language
         }
      }
 end
+programmer_hash.collect do |:grace_hopper, known_for, languages|
 
 def adding_matz
 # add the following information to the top level of programmer_hash
